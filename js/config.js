0280163dd525c347bd752a80cc4944207595ba97
Román SG <roman.sosa@atos.net>
2014-11-12 16:14:42 +0000

    Removed unnecessary js code. Added SLADASHBOARD_ENDPOINT variable.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 /*
     Live demo configuration
 
-    - This file is dependant on where Brooklyn is mounted and needs to
+    - This file is dependant on where Brooklyn and Sla is mounted and needs to
     be changed.
 */
 
 var BROOKLYN_ENDPOINT = "http://192.168.24.128:8081";
+var SLADASHBOARD_ENDPOINT = "http://localhost:8082";
